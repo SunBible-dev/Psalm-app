df0f98661771a03fcb610635af0402f3bac658b3
The SunShining <thesunshiningmin@gmail.com>
2021-01-07 12:11:25 -0600

    Start

---
+++ main.js
@@ -0,0 +1,102 @@
+//I have no idea what most of this does!!! - NCR
+
+const { app, BrowserWindow, session, Menu } = require('electron')
+
+
+// Main App Activity: 
+app.on('ready', async () => {
+
+
+    const mainWindow = new BrowserWindow({
+        minimizable: true,
+        width: 1000,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            webviewTag: true
+        }
+    })
+
+    mainWindow.loadFile('Psalm-app-iframe.html')
+    mainWindow.removeMenu()
+
+
+
+})
+
+
+
+//App menu mac os
+const isMac = process.platform === 'darwin'
+
+const template = [
+  // { role: 'appMenu' }
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [
+      {
+        label: 'View SunBible Online',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://the-sunshining.github.io/SunBible/')
+        }
+      },
+      {
+        label: 'Open SunBible Wallpaper online',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://the-sunshining.github.io/SunBible_IMG_Library/SunBible_wallpaper_img/wallpaper.html')
+        }
+      },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideothers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'about' },
+      { role: 'quit' }
+    ]
+  }] : []),
+ 
+  
+  // { role: 'viewMenu' }
+  ...(isMac ? [{
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  }] : [])
+ 
+  
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
+
+
+
+
+
+// Right Click Menu using - "electron-context-menu": "^2.3.0"
+const contextMenu = require('electron-context-menu');
+contextMenu({
+    menu: (actions, props, browserWindow) => [
+
+        { role: 'copy' },
+        { role: 'selectAll' },
+        { type: 'separator' },
+        actions.copyImage(),
+        actions.saveImageAs(),
+        { type: 'separator' },
+        { role: 'minimize'},
+        { role: 'togglefullscreen' },
+        { role: 'close' }
+
+    ]
+});
+
+
+
+
